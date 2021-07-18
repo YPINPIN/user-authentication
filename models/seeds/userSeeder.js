807db3181a60bf21e1979cf55bb278a562ef1844
YPINPIN <yping611@gmail.com>
2021-07-18 20:54:15 +0800

    [ADD] Add user schema and seeder

---
+++ models/seeds/userSeeder.js
@@ -0,0 +1,45 @@
+const User = require('../users')
+// mongoose
+const db = require('../../config/mongoose')
+
+// 測試種子資料
+const users = [
+  {
+    firstName: 'Tony',
+    email: 'tony@stark.com',
+    password: 'iamironman',
+  },
+  {
+    firstName: 'Steve',
+    email: 'captain@hotmail.com',
+    password: 'icandothisallday',
+  },
+  {
+    firstName: 'Peter',
+    email: 'peter@parker.com',
+    password: 'enajyram',
+  },
+  {
+    firstName: 'Natasha',
+    email: 'natasha@gamil.com',
+    password: '*parol#@$!',
+  },
+  {
+    firstName: 'Nick',
+    email: 'nick@shield.com',
+    password: 'password',
+  },
+]
+
+// 連線成功
+db.once('open', () => {
+  // 建立資料
+  users.forEach((user) => {
+    User.create({
+      firstName: user.firstName,
+      email: user.email,
+      password: user.password,
+    })
+  })
+  console.log('userSeeder done!')
+})
---
+++ models/users.js
@@ -0,0 +1,22 @@
+// mongoose
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+})
+
+module.exports = mongoose.model('User', userSchema)
