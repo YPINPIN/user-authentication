c2e03955828b1a93fd9656efb17411768979b985
YPINPIN <yping611@gmail.com>
2021-07-18 22:14:14 +0800

    [ADD] Add welcome page and get user

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,6 +2,8 @@
 const express = require('express')
 // 路由器
 const router = express.Router()
+// user model
+const User = require('../../models/user')
 
 // 首頁
 router.get('/', (req, res) => {
@@ -10,8 +12,18 @@ router.get('/', (req, res) => {
 
 // 登入
 router.post('/login', (req, res) => {
-  console.log(req.body)
-  res.render('index')
+  const { email, password } = req.body
+  User.findOne({ email, password })
+    .lean()
+    .then((user) => {
+      if (!user) {
+        const loginFault = true
+        res.render('index', { email, password, loginFault })
+      } else {
+        res.render('welcome', { user })
+      }
+    })
+    .catch((error) => console.error(error))
 })
 
 // 匯出路由器
